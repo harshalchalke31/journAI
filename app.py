2d98769043126034d436c4f33b548d128f924a22
harshalchalke31 <harshal.chalke2001@gmail.com>
2025-01-15 13:49:44 -0500

    working

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from langchain.prompts import PromptTemplate
-from langchain.llms import ctransformers
-
+# from langchain_community.llms import CTransformers
+from ctransformers import AutoModelForCausalLM
+import os
+# print(f'\nWorking Directory: {os.getcwd()}\n\n\n')
 ## Core function: llama
 
 def llama_llm(topic_name, content_type, word_count,content_style,description):
     
-    # model
-    llm = ctransformers(model = 'C:\Projects\python\journAI\models\llama-2-7b-chat.ggmlv3.q8_0.bin',
-                        model_type = 'llama',
-                        config = {'max_new_tokens':100,
-                                'temperature':0.1})
+    # model #'C:\Projects\python\journAI\models\llama-2-7b-chat.ggmlv3.q8_0.bin',
+    # llm = AutoModelForCausalLM.from_pretrained(model = os.path.join(os.getcwd(),'models','llama-2-7b-chat.ggmlv3.q8_0.bin'),
+    #                             model_type = 'llama',  
+    #                             config = {'max_new_tokens':100,'temperature':0.1})
+
+    llm = AutoModelForCausalLM.from_pretrained(model_path_or_repo_id=os.path.join(os.getcwd(),'models','llama-2-7b-chat.ggmlv3.q8_0.bin'),
+                                                model_type='llama')
     ## prompt template
     template="""Write a {content_type} for the topic {topic_name}. 
     It is being written for {content_style}, for the description {description}. 
@@ -19,12 +23,15 @@ def llama_llm(topic_name, content_type, word_count,content_style,description):
     prompt = PromptTemplate(input_variables=['content_type','topic_name','content_style','description','word_count'],
                             template=template)
 
-    # generate response
-    response = llm(prompt.format(content_type = content_type,
+    formatted_prompt = prompt.format(content_type = content_type,
                     topic_name = topic_name,
                     content_style=content_style,
                     description=description,
-                    word_count=word_count))
+                    word_count=word_count)
+    
+    # print(f'\n\n\nFormatted prompt: {formatted_prompt}\n\n\n')
+    # generate response
+    response = llm(formatted_prompt,stream=True,temperature=0.1)
     return response
 
 
@@ -62,12 +69,12 @@ with col3:
                                         options=('Self'),
                                         index=0)
 
-description = st.text_input(label='Elaborate on your topic',max_chars=1000)
+description = st.text_area(label='Elaborate on your topic',max_chars=1000)
 submit = st.button('Generate ->')
 
 ## Final response
 if submit:
-    st.write_stream(llama_llm(topic_name=topic_name,
+    st.write(llama_llm(topic_name=topic_name,
                                 content_type=content_type,
                                 content_style=content_style,
                                 word_count=word_count,
