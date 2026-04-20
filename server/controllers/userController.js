23494398e695b77a54dbb1111ac6725a86c04614
harshalchalke31 <harshal.chalke2001@gmail.com>
2026-04-20 17:20:13 -0400

    api created for viewing published images

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
+import Chat from '../models/Chat.js'
 
 // Generate JWT for authetication of new user
 const generateToken = (id) => {
@@ -59,5 +60,30 @@ export const getUser = async (req, res) => {
     } catch (error) {
         return res.json({ success: false, message: error.message })
 
+    }
+}
+
+//API to get published images
+export const getPublishedImages = async(req, res)=>{
+    try{
+        const publishedImageMessages= await Chat.aggregate([
+            {$unwind:"$messages"},
+            {
+                $match:{
+                    "messages.isImage":true,
+                    "messages.isPublished": true,
+                }
+            },
+            {
+                $project:{
+                    _id:0,
+                    imageUrl:"$messages.content",
+                    userName: "$userName",
+                }
+            }
+        ])
+        res.json({success:true, images: publishedImageMessages.reverse()})
+    }catch(error){
+        res.json({success:false, message:error.message})
     }
 }
